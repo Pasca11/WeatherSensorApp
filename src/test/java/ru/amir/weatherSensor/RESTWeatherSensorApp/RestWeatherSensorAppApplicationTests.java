96ad45af00e8c7153af95b162df5136aa77d19ca
Pasca11 <dr.marshall.work@gmail.com>
2023-07-26 15:40:57 +0300

    first commit

---
+++ src/test/java/ru/amir/weatherSensor/RESTWeatherSensorApp/RestWeatherSensorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.amir.weatherSensor.RESTWeatherSensorApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWeatherSensorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
